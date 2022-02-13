fe9101175596732e0b84386667310929ff722fbd
Dipesh Silwal <75604769+dipesg@users.noreply.github.com>
2022-02-13 12:09:17 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import tensorflow as tf
+import tensorflow
 tf.compat.v1.get_default_graph
 from tensorflow.keras import backend
-from keras.application.keras_vggface.utils import preprocess_input
+from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
 from src.utils.all_utils import read_yaml, create_directory
 import pickle
